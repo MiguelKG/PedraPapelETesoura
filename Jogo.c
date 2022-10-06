b2edf05edadaa864b7143bc1704d8c01eae62483
Miguel <m>
2022-10-06 17:52:07 -0300

    Pedra, Papel, Tesoura, Lagarto e Spock

--- Jogo.c
+++ Jogo.c
@@ -3,19 +3,19 @@
 #include <time.h>
 
 int main () {
-    int act = 0, playerAct = 0, enemyAct = 0, ctn = 1, p[3][2] = {{3, 2}, {1, 3}, {2, 1}};
+    int act = 0, playerAct = 0, enemyAct = 0, ctn = 1, p[5][2] = {{3, 4}, {1, 5}, {2, 4}, {2, 5}, {1, 3}};
     srand(time(NULL));
 
     while (ctn == 1) {
-        printf("\nBem-vindo ao Pedra, Papel e Tesoura!\n"
-        "Escolha sua jogada:\n1 - Pedra\n2 - Papel\n3 - Tesoura\n");
+        printf("\nBem-vindo ao Pedra, Papel, Tesoura, Lagarto e Spock!\n"
+        "Escolha sua jogada:\n1 - Pedra\n2 - Papel\n3 - Tesoura\n4 - Lagarto\n5 - Spock\n");
         scanf("%d", &playerAct);
-        while (playerAct < 1 || playerAct > 3) {
+        while (playerAct < 1 || playerAct > 5) {
             printf("Valor inválido, por favor, informe novamente sua jogada\n");
             scanf("%d", &playerAct);
         }
         printf("\n");
-        enemyAct = ((rand() % 3) + 1);
+        enemyAct = ((rand() % 4) + 1);
 
         for(int i = 0; i < 2; i++) {
             if (i == 0) {
@@ -36,13 +36,19 @@ int main () {
                 case 3:
                     printf("tesoura!\n");
                     break;
+                case 4:
+                    printf("lagarto!\n");
+                    break;
+                case 5:
+                    printf("Spock!\n");
+                    break;
             }
         }
 
         if (playerAct == enemyAct) {
             printf("Empate\n");
         } else {
-            if (p[playerAct - 1][0] == enemyAct) {
+            if (p[playerAct - 1][0] == enemyAct || p[playerAct -1][1] == enemyAct) {
                 printf("Você venceu\n");
             } else {
                 printf("Você perdeu\n");
